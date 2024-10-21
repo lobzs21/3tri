e9f0dbb1fb66c0dff0fd313904e9d2ba97875e38
lobzs21 <127985821+lobzs21@users.noreply.github.com>
2024-10-21 15:17:35 -0300

    Add files via upload

--- Graficos/InfomacoesGlobais.js
+++ Graficos/InfomacoesGlobais.js
@@ -5,9 +5,10 @@ async function visualizarInformacoesGlobais() {
     const dados = await res.json()
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Vocẽ sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e 
-    que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e 
-    pasam em média ${dados.tempo_medio} horas conectadas.`
+   
+    paragrafo.innerHTML = `Vocẽ sabia que o mundo tem <span>${dados.total_pessoas_mundo}>/span< de pessoas e 
+    que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e 
+    pasam em média <span>${dados.tempo_medio}</span> horas conectadas.`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
